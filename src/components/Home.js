aab292f07af1afb44f2b0443d32d68361f71d58c
Nikhil Deore <nikhildeore05@gmail.com>
2023-03-30 15:04:41 +0530

    initial

---
+++ src/components/Home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className="bg-image">
+        <span><h1 id="heading"style={{fontSize:'75px', textAlign:'center', marginTop:'10%'}}>INT252</h1></span>
+    </div>
+  )
+}
